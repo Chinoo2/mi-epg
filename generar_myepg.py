9f3dde11e439b4960c53207e9a215277de4cfc34
Chinoo2 <trabajocompu29@gmail.com>
2026-07-24 16:48:32 -0300

    Update generar_myepg.py

--- generar_myepg.py
+++ generar_myepg.py
@@ -1,46 +1,67 @@
 from datetime import datetime, timedelta
 
 canales = [
-    ("MI_MUSICA_ROMANTICA", "100% Amor"),
-    ("MI_MUSICA_REGGAETON", "Puro Flow"),
-    ("MI_MUSICA_SALSA", "Musica Salsa"),
-    ("FOLKLORICO", "Musica folklorica chamamé"),
-    ("TIERRA_MIA_TV", "24 horas Chamamé"),
-    ("BILLBOARD", "Billboard")
+    ("MI_MUSICA_ROMANTICA", "Mi Musica Romantica", "100% Amor"),
+    ("MI_MUSICA_REGGAETON", "Mi Musica Reggaeton", "Puro Flow"),
+    ("MI_MUSICA_SALSA", "Mi Musica Salsa", "Musica Salsa"),
+    ("FOLKLORICO", "Folklorico", "Musica folklorica chamamé"),
+    ("TIERRA_MIA_TV", "Tierra Mia TV", "24 horas Chamamé"),
+    ("BILLBOARD", "Billboard", "Billboard")
 ]
 
-inicio = datetime.utcnow().replace(hour=0, minute=0, second=0)
 
-xml = '''<?xml version="1.0" encoding="UTF-8"?>
+fecha_inicio = datetime.utcnow().replace(
+    hour=0,
+    minute=0,
+    second=0,
+    microsecond=0
+)
+
+
+xml = """<?xml version="1.0" encoding="UTF-8"?>
 <tv generator-info-name="EPG Automatico">
-'''
+"""
 
-for canal, nombre in canales:
-    xml += f'''
-<channel id="{canal}">
-<display-name>{canal}</display-name>
+
+# Crear canales
+
+for canal_id, nombre, programa in canales:
+    xml += f"""
+<channel id="{canal_id}">
+    <display-name>{nombre}</display-name>
 </channel>
-'''
+"""
 
-for canal, programa in canales:
-    for dia in range(30):
-        start = inicio + timedelta(days=dia)
-        stop = start + timedelta(days=1)
 
-        xml += f'''
-<programme channel="{canal}" 
-start="{start.strftime("%Y%m%d%H%M%S")} +0000" 
-stop="{stop.strftime("%Y%m%d%H%M%S")} +0000">
+# Crear programación 30 días
 
-<title>{programa}</title>
-<desc>Programacion 24 horas</desc>
+for dia in range(30):
 
+    inicio = fecha_inicio + timedelta(days=dia)
+    fin = inicio + timedelta(days=1)
+
+    start = inicio.strftime("%Y%m%d%H%M%S")
+    stop = fin.strftime("%Y%m%d%H%M%S")
+
+    for canal_id, nombre, programa in canales:
+
+        xml += f"""
+<programme channel="{canal_id}" start="{start} +0000" stop="{stop} +0000">
+    <title>{programa}</title>
+    <desc>Programacion musical 24 horas</desc>
 </programme>
-'''
+"""
 
-xml += "\n</tv>"
 
-with open("epg.xml","w",encoding="utf-8") as f:
-    f.write(xml)
+xml += """
+</tv>
+"""
 
-print("EPG actualizado")
+
+# Guardar archivo
+
+with open("epgmy.xml", "w", encoding="utf-8") as archivo:
+    archivo.write(xml)
+
+
+print("EPG actualizado correctamente")
